3cbd1d07bd430f4a5ffec9f56013f74391d94d55
ATheBugoo <andrewsellers04@gmail.com>
2023-10-21 22:37:28 -0400

    impromptu merge, fixed small potential issue with pedestrian speed.

--- assets/pedestrians/MoveComponent/MoveComponent.gd
+++ assets/pedestrians/MoveComponent/MoveComponent.gd
@@ -3,4 +3,4 @@ class_name MoveComponent
 
 #pedSpeed should be in hundreds (250, 300, etc.) distance in 10s and initDistance in 10s
 func calcMovement(pedSpeed, distance, initDistance) -> int:
-	return ((100 * pedSpeed) / (distance - (initDistance / 2)));
+	return ((100 * pedSpeed) / (distance + (initDistance / 2)));
