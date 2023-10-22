0aa9af2d680d2146c76c0a1cafd45d788023043b
qapples <armand.scotty@yahoo.com>
2023-10-22 04:27:35 -0400

    get figure to ragdoll on hit.

--- assets/pedestrians/FatPedestrian/FatPedestrian.gd
+++ assets/pedestrians/FatPedestrian/FatPedestrian.gd
@@ -5,8 +5,8 @@ var moveSpeed;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var pedSpeedMin = 50;
-	var pedSpeedMax = 100;
+	var pedSpeedMin = 10
+	var pedSpeedMax = 50;
 	var distance = 40; 
 	var initDistance = randi_range(0, distance / 2); #0 is placeholder
 	moveSpeed = moveComponent.calcMovement(randi_range(pedSpeedMin, pedSpeedMax), 
--- pedestrian.gd
+++ pedestrian.gd
@@ -2,14 +2,32 @@ extends RigidBody3D
 
 @onready var anim_player = get_node("AnimationPlayer")
 
+var dead = false;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#anim_player.play("Running")
+	anim_player.play("Running")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if not dead:
+		return
 	
-func _on_body_entered(body:Node):
-	print(body, " entered")
+	#linear_velocity.y = -1.0
+	
+func _on_body_entered(body: Node):
+	if not body is VehicleBody3D:
+		return
+	
+	var figure: MeshInstance3D = $"Armature/Figure"
+	var ragdoll_skeleton: Skeleton3D = $"Armature/RagdollSkeleton"
+	var anim_player = $"AnimationPlayer"
+	
+	anim_player.stop()
+	figure.set_skeleton_path("../RagdollSkeleton")
+	ragdoll_skeleton.physical_bones_start_simulation()
+	
+	linear_velocity = Vector3(0, 0, 0)
+	angular_velocity = Vector3(0, 0, 0)
+	
+	var dead = true
