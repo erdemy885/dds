7bb08546e852f8950f4336110195cd8c6af4c5a2
Qapples <qapples@vt.edu>
2023-10-22 01:05:39 -0400

    add ragdolls

--- assets/pedestrians/FatPedestrian/FatPedestrian.gd
+++ assets/pedestrians/FatPedestrian/FatPedestrian.gd
@@ -1,12 +1,12 @@
-extends RigidBody3D
+extends Node3D
 var moveSpeed;
 
 @export var moveComponent : MoveComponent
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var pedSpeedMin = 275;
-	var pedSpeedMax = 300;
+	var pedSpeedMin = 50;
+	var pedSpeedMax = 100;
 	var distance = 40; 
 	var initDistance = randi_range(0, distance / 2); #0 is placeholder
 	moveSpeed = moveComponent.calcMovement(randi_range(pedSpeedMin, pedSpeedMax), 
@@ -15,4 +15,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	apply_central_force(moveSpeed * Vector3.FORWARD * delta);
+	get_parent().apply_central_force(moveSpeed * Vector3.FORWARD * delta);
---
+++ pedestrian.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+@onready var anim_player = get_node("AnimationPlayer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim_player.play("Running")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _on_body_entered(body:Node):
+	print(body, " entered")
